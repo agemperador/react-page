d39c3287893f179d4e6394838fbecf1594f38b33
aemperador <emperadoragustin@gmail.com>
2020-05-21 06:46:13 -0300

    Updates

---
+++ precache-manifest.2b7a16b4125818899d95980321000498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58620a1801edadf060fadbde6bfca4f",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "cdb0fc8a3c479950450c",
+    "url": "/react-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c25a843cf108bb52e111",
+    "url": "/react-page/static/js/2.5cc60400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.5cc60400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb0fc8a3c479950450c",
+    "url": "/react-page/static/js/main.efc12333.chunk.js"
+  },
+  {
+    "revision": "dda49fd40105236bfc9c",
+    "url": "/react-page/static/js/runtime-main.6a2f6f15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
